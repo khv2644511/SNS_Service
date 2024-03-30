ef33a21f47712dd38220524a9c3b5cb07469298e
khv2644511 <khv2644511@naver.com>
2024-03-30 09:28:00 +0900

    Style: PostForm 만들기

--- hyeppy-sns/src/app/(afterLogin)/home/page.tsx
+++ hyeppy-sns/src/app/(afterLogin)/home/page.tsx
@@ -2,13 +2,14 @@ import style from './home.module.css';
 
 import Tab from './_component/Tab';
 import TabProvider from './_component/TabProvier';
+import PostForm from './_component/PostForm';
 
 export default function Home() {
     return (
         <main className={style.main}>
             <TabProvider>
                 <Tab />
-                {/* <PostFrom /> */}
+                <PostForm />
                 {/* <Post /> */}
             </TabProvider>
         </main>
